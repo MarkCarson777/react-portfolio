a64281b01e451689d349fe7d3e07bbcc7abf1c24
Mark Carson <markcarson121@gmail.com>
2021-09-03 15:04:14 +0100

    Updates

---
+++ static/js/main.2a9bd0a1.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-portfolio"]=this["webpackJsonpreact-portfolio"]||[]).push([[0],{29:function(e,t,c){"use strict";c.r(t);c(0);var o=c(12),r=c.n(o),n=c(13),i=c(1),a=c(6),s=function(){return Object(a.jsx)("div",{children:Object(a.jsx)("h2",{children:"React Portfolio"})})},j=function(){return Object(a.jsx)("div",{children:Object(a.jsx)(n.a,{basename:"/react-portfolio",children:Object(a.jsx)(i.a,{path:"/",exact:!0,component:s})})})};r.a.render(Object(a.jsx)(j,{}),document.querySelector("#root"))}},[[29,1,2]]]);
+//# sourceMappingURL=main.2a9bd0a1.chunk.js.map
