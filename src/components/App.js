8d454ebe871a711ea7dbf05eb3e5e62afb293b0b
Mark Carson <markcarson121@gmail.com>
2021-09-03 15:03:35 +0100

    created App.js

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Homepage = () => {
+  return (
+    <div>
+      <h2>React Portfolio</h2>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter basename="/react-portfolio">
+        <Route path="/" exact component={Homepage} />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
