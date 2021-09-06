0664315b1bd8af2c3a91529ba9ffe6df826c7c78
Mark Carson <markcarson121@gmail.com>
2021-09-06 10:34:37 +0100

    built Employment.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 // import Route from './Route';
 import NavBar from './NavBar';
 import Profile from './Profile'
+import Employment from './Employment';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 const Homepage = () => {
@@ -19,6 +20,7 @@ const App = () => {
       <BrowserRouter basename="/react-portfolio">
         <Route path="/" exact component={Homepage} />
         <Route path="/profile" exact component={Profile} />
+        <Route path="/employment" exact component={Employment} />
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Employment.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from './NavBar';
+
+const Employment = () => {
+  return (
+    <div>
+      <NavBar />
+      <h2>Employment History</h2>
+    </div>
+  );
+};
+
+export default Employment;
